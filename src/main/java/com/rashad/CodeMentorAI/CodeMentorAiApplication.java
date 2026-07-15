96a42079a919f1729df93708170b4e055c458357
Mohd Rashad <youremail@example.com>
2026-07-15 23:01:14 +0530

    Initial Spring Boot setup

---
+++ src/main/java/com/rashad/CodeMentorAI/CodeMentorAiApplication.java
@@ -0,0 +1,13 @@
+package com.rashad.CodeMentorAI;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CodeMentorAiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CodeMentorAiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/rashad/CodeMentorAI/CodeMentorAiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rashad.CodeMentorAI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CodeMentorAiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
